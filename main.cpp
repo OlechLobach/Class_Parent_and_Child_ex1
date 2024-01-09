544f8e9f8de94af48429faac53afd4c0af59f89d
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-01-09 19:53:20 +0200

    initiall commit

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main() {
     Student s1("Alice", 20);
     Aspirant a1("Bob", 25, "Machine Learning");
 
-    std::cout << "Student: " << s1.getName() << ", Age: " << s1.getAge() << std::endl;
-    std::cout << "Aspirant: " << a1.getName() << ", Age: " << a1.getAge() << ", Thesis Topic: " << a1.getThesisTopic() << std::endl;
+    cout << "Student: " << s1.getName() << ", Age: " << s1.getAge() << endl;
+    cout << "Aspirant: " << a1.getName() << ", Age: " << a1.getAge() << ", Thesis Topic: " << a1.getThesisTopic() << endl;
 
     return 0;
 }
